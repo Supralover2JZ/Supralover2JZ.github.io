8bb82efec5095ba62eae04e3b0e2b991392a56dd
Supralover2JZ <supralover6664life@gmail.com>
2026-05-04 14:20:38 +0200

    game.js

--- game.js
+++ game.js
@@ -1,1 +1,6 @@
-yoo
+const shopBtn = document.getElementById("shopBtn");
+const shop = document.getElementById("shop");
+
+shopBtn.addEventListener("click", () => {
+  shop.style.display = "block";
+});
